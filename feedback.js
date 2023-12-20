5629285e8768cb7ce3a9bf32d69a7f75b283ace0
wojtek-strzyzowski <151506991+wojtek-strzyzowski@users.noreply.github.com>
2023-12-20 11:14:06 +0100

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,10 @@
+function gut() {
+    let smileIcon = document.getElementById("smile");
+
+    
+    smileIcon.classList.remove("bi bi-emoji-smile-fill");
+
+   
+    smileIcon.classList.toggle("bi-emoji-frown-fill");
+}
+
