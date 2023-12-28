5baee573b2b0d252e93a393a95767c6325b5b5f3
wojtek-strzyzowski <151506991+wojtek-strzyzowski@users.noreply.github.com>
2023-12-28 18:53:56 +0100

    mit unterschrift in grün und rot

--- feedback.js
+++ feedback.js
@@ -7,14 +7,19 @@ function switchEmoji(feeling){
         smileIcon.classList.remove("bi-emoji-frown-fill");
         smileIcon.classList.remove("bi-emoji-neutral-fill");
 
-        smileIcon.classList.add("bi-emoji-smile-fill");
+        smileIcon.classList.add("bi-emoji-heart-eyes");
+        document.getElementById("antwort").innerHTML = "Thank you very much, that makes me happy!";
+        document.getElementById("antwort").style.color = "green";
     }
 
     else if(feeling == "schlecht") {
         
-        smileIcon.classList.remove("bi-emoji-smile-fill");
+        smileIcon.classList.remove("bi-emoji-heart-eyes");
         smileIcon.classList.remove("bi-emoji-neutral-fill");
 
         smileIcon.classList.add("bi-emoji-frown-fill");
+        document.getElementById("antwort").innerHTML = "OH NOOO I`m sorry to hear that!!";
+        document.getElementById("antwort").style.color = "red";
+
     }
 }
